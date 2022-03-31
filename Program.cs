3a7653fe574cc42f5e20158150547e11397d30f4
Douglas Vinicius Vicente <douglas.vinicius001@gmail.com>
2022-03-30 22:27:09 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+namespace ULTIMOTESTE_4
+{
+      class program
+      {
+            public static void Main (string []args)
+            {
+                  WriteLine("Informe o valor da compra: ");
+                  int ValorCompra = int.Parse(ReadLine());
+
+                  if (ValorCompra >=300 && ValorCompra <=2200)
+                  {
+                  int Porcento = ValorCompra - 200;
+                  int Desconto = Porcento / 100;
+                  int DescontoLiquido = (ValorCompra * Desconto)/100;
+                  int Total = ValorCompra - DescontoLiquido;
+
+                  WriteLine($"O valor da compra é: {ValorCompra}"+ $"\nPorcentagem de desconto adquirida:  {Desconto}%"+$"\nValor Final: {Total}");
+                  }
+                  else if (ValorCompra > 2200)
+                  {
+                        int Resultado = (ValorCompra * 20)/100;
+                        Resultado = ValorCompra - Resultado;
+                        WriteLine($"O valor da compra é: {ValorCompra}"+ $"\nPorcentagem de desconto adquirida:  20%"+$"\nValor Final: {Resultado}");
+                  }
+                  else if (ValorCompra <300)
+                  {
+                        WriteLine("Infelizmente você não adquiriu descontos :(");
+                  }
+
+
+            }     
+      }
+}
